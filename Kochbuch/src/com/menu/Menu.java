ca5e005ff54bb86cfd52c58c79589157b09e74c8
kevin <kevinnoll90@gmail.com>
2013-09-06 16:26:30 +0200

    klasse menu hinzugefügt, bildet ein Menü ab

---
+++ Kochbuch/src/com/menu/Menu.java
@@ -0,0 +1,46 @@
+package com.menu;
+
+import com.receipt.Receipt;
+
+public class Menu {
+
+	private String name;
+	private Receipt starter;
+	private Receipt main;
+	private Receipt dessert;
+	
+	public Menu(String name, Receipt starter, Receipt main, Receipt dessert) {
+		super();
+		this.name = name;
+		this.starter = starter;
+		this.main = main;
+		this.dessert = dessert;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Receipt getStarter() {
+		return starter;
+	}
+	public void setStarter(Receipt starter) {
+		this.starter = starter;
+	}
+	public Receipt getMain() {
+		return main;
+	}
+	public void setMain(Receipt main) {
+		this.main = main;
+	}
+	public Receipt getDessert() {
+		return dessert;
+	}
+	public void setDessert(Receipt dessert) {
+		this.dessert = dessert;
+	}
+	
+	
+}
